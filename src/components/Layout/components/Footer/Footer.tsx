21b4359055159a6a004cc2810c076f503434fdbf
Табалов Никита <nikitatabalov@gmail.com>
2023-10-12 04:18:14 +0300

    footer update

--- src/components/Layout/components/Footer/Footer.tsx
+++ src/components/Layout/components/Footer/Footer.tsx
@@ -1,23 +1,28 @@
 import { FC, memo } from 'react';
 import styles from './Footer.module.scss';
+import { FaTelegram, FaYoutube, FaInstagram } from 'react-icons/fa6';
 import { ILink } from '../../../../types/ILink';
 import Logo from '../../../Logo/Logo';
 import { v4 } from 'uuid';
 
 interface IFooterProps {
 	links: ILink[];
+	phones: string[];
+	emails: string[];
 }
 
-const Footer: FC<IFooterProps> = memo(({ links }) => {
+const Footer: FC<IFooterProps> = memo(({ links, phones, emails }) => {
 	const date = new Date().getFullYear();
 	return (
 		<footer className={styles.footer}>
 			<div className={styles.FooterContainer}>
 				<div className={styles.mainBox}>
 					<ul className={styles.optionsCol}>
+						<div className={styles.colHeading}>Навигация</div>
+						<div className={styles.divider} />
 						{links.map((link) => {
 							return (
-								<li key={v4()}>
+								<li className={styles.option} key={v4()}>
 									<a href={link.link} className={styles.link}>
 										{link.heading}
 									</a>
@@ -25,8 +30,39 @@ const Footer: FC<IFooterProps> = memo(({ links }) => {
 							);
 						})}
 					</ul>
-					<ul className={styles.contactsCol}></ul>
-					<div className={styles.otherCol}>{/* <Logo /> */}</div>
+
+					<ul className={styles.contactsCol}>
+						<div className={styles.colHeading}>Контакты</div>
+						<div className={styles.divider} />
+						{phones.map((phone) => {
+							return (
+								<li className={styles.contact} key={v4()}>
+									{phone}
+								</li>
+							);
+						})}
+						{emails.map((email) => {
+							return (
+								<li className={styles.contact} key={v4()}>
+									{email}
+								</li>
+							);
+						})}
+					</ul>
+					<ul className={styles.linksCol}>
+						<div className={styles.colHeading}>Наши соцсети</div>
+						<div className={styles.divider} />
+						<li className={styles.outLink}>
+							<FaTelegram />
+						</li>
+						<li className={styles.outLink}>
+							<FaYoutube />
+						</li>
+						<li className={styles.outLink}>
+							<FaInstagram />
+						</li>
+					</ul>
+					{/* <div className={styles.otherCol}><Logo /></div> */}
 				</div>
 			</div>
 			<div className={styles.copyright}>
