57c5491c7e1fc6c91ce60a703232936f5838a4e3
Табалов Никита <nikitatabalov@gmail.com>
2023-10-12 04:20:46 +0300

    ThemeProvider added to Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import { FC, PropsWithChildren, memo } from 'react';
 import styles from './layout.module.scss';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import ThemeProvider from '../ThemeProvider/ThemeProvider';
 
 const Layout: FC<PropsWithChildren> = memo(({ children }) => {
 	const links = [
@@ -21,16 +22,21 @@ const Layout: FC<PropsWithChildren> = memo(({ children }) => {
 			heading: 'Контакты',
 			link: '#contacts',
 		},
-		// {
-		// 	heading: 'Партнерам',
-		// 	link: '/',
-		// },
 	];
 	return (
 		<div className={styles.layout}>
-			<Header links={links} />
-			{children}
-			<Footer links={links} />
+			<ThemeProvider>
+				<Header links={links} />
+				{children}
+				<Footer
+					links={links}
+					phones={['+7 926 (082) 09-75', '+7 926 (082) 09-75']}
+					emails={[
+						'nikitatabalov@gmail.com',
+						'nikitatabalov@gmail.com',
+					]}
+				/>
+			</ThemeProvider>
 		</div>
 	);
 });
