3c7b3d9a437d21a10cc347e09b8bcad0a48f8e6a
Табалов Никита <nikitatabalov@gmail.com>
2023-10-12 04:18:31 +0300

    glass UI button added

---
+++ src/GlassUI/Button/Button.tsx
@@ -0,0 +1,33 @@
+import { FC, PropsWithChildren } from 'react';
+import styles from './Button.module.scss';
+import { classNames } from '../../components/utils/classNames';
+
+interface BaseButtonProps {}
+
+type MergedHTMLAttributes = Omit<
+	React.HTMLAttributes<HTMLElement> &
+		React.ButtonHTMLAttributes<HTMLElement> &
+		React.AnchorHTMLAttributes<HTMLElement>,
+	'type'
+>;
+
+interface IButtonProps extends BaseButtonProps, MergedHTMLAttributes {}
+
+const Button: FC<PropsWithChildren<IButtonProps>> = ({
+	children,
+	className,
+	...rest
+}) => {
+	return (
+		<button
+			className={classNames(styles.Button, {}, [
+				className ? className : '',
+			])}
+			{...rest}
+		>
+			{children}
+		</button>
+	);
+};
+
+export default Button;
